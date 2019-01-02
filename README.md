667888f071f79e251ea4d52c4d1b9a3d03e3f34f
David Gómez <davidjgomez@gmail.com>
2019-01-01 19:50:49 -0500

    Solution to the exercise

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# nodeJS-helloWorldAPI
-A Hello World API that answers when posting to /hello
+# Hello World API
+
+This is the assignment 1 for Pirple's NodeJS Master Class
+
+## Requirements
+
+Please create a simple "Hello World" API. Meaning:
+
+1. It should be a RESTful JSON API that listens on a port of your choice. 
+2. When someone posts anything to the route /hello, you should return a welcome message, in JSON format. This message can be anything you want. 
+
+## Solution
+
+1. A simple server listening on port 3000
+2. Responds with a welcoming message when requesting to /hello
+3. Responds with a no so welcoming message otherwise
+4. Just uses the _http_ and _url_ libraries due to the simple requirements
+5. Does not process any sent query strings but is capable to recognize the /hello request in these cases too
+5. Does not process any sent payload or headers
+
+## Instructions
+
+Just run it with _node index.js_ and do some requests to _localhost:3000_ with _/hello_ and something else as well
---
+++ index.js
@@ -0,0 +1,66 @@
+/* 
+ * Hello World API
+ *
+ */
+
+// Dependencies
+const http = require('http')
+const url = require('url')
+
+// Instantiate the HTTP server
+const server = http.createServer((req, res) => {
+    
+    // Get the URL and parse it
+    const parsedUrl = url.parse(req.url, false)
+
+    // Get the trimmed path
+    const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '')
+
+    req.on('data', () => {})
+    req.on('end', () => {
+        
+        // Choose the handler this request should go to. If one is not found, use the Not Found handler
+        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound
+
+        // Route the request to the handler specified in the router
+        chosenHandler((statusCode, payload) => {
+            // Use the status code called back by the handler, or default to 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200
+
+            // Use the payload called back by the handler, or default to an empty object
+            payload = typeof(payload) == 'object' ? payload : {}
+
+            // Convert the payload to a string
+            const payloadString = JSON.stringify(payload) 
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json')
+            res.writeHead(statusCode)
+            res.end(payloadString)  
+            
+            // Log the request path
+            console.log('Returning this response: ', statusCode, payloadString)
+        })
+    })
+})
+
+// Start the HTTP server
+server.listen(3000, () => console.log('The server is listening on port 3000'))
+
+// Define the handlers
+const handlers = {}
+
+// Ping handler
+handlers.hello = (callback) => {
+    callback(200, { 'message' : 'Hello! Thanks for saying hi!' })
+}
+
+// Not Found handler
+handlers.notFound = (callback) => {
+    callback(404, { 'message' : 'Don\'t be so impolite next time and say hello first!' })
+}
+
+// Define a request router
+const router = {
+    'hello' : handlers.hello
+}
